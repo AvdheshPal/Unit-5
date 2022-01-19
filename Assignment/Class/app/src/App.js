617e0a77440f835c8c4fa454e9cadf0f6ad2c4cc
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-01-20 02:47:47 +0530

    k

--- Assignment/Class/app/src/App.js
+++ Assignment/Class/app/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {useSelector , useDispatch} from 'react-redux'
+import { useState } from 'react';
 import {addTodo} from './Redux/Action'
 import {Todos} from './componants/Todos'
-import ReactPlyaer from 'react-player'
-import { IconName } from "react-icons/fc";
+import {Profile} from './componants/Profile'
+import {Glogin,Glogout} from './componants/GoogleOauth'
 
 
 function App() {
   const data = useSelector(state => state.todos)
   const dispatch = useDispatch();
+
+  
+
   return (
     <div className="App">
       <h1 className="font-bold text-4xl mb-6 text-indigo-600" >This is React App with Tailwind</h1>
@@ -18,8 +22,9 @@ function App() {
      }} >Add 1</button>
      <button>Add -1</button> */}
      <Todos/>
-     <ReactPlyaer url="https://www.w3schools.com/html/movie.mp4" />
-     <IconName/>
+     <Glogin/>
+     <Glogout/>
+     <Profile/>
     </div>
   );
 }
--- Assignment/Class/app/tailwind.config.js
+++ Assignment/Class/app/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('https://www.mento.co.in/assets/all-topics-bg-c70ac523da68e4c48726cc16261ddb6d5b08a7f43fa4dc0e78f5488466e78514.png')"
+      }
+    },
   },
   plugins: [],
 }
