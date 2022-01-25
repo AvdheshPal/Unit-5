09b3a7339d8acacd7486bcabf02f8b75b567f2e8
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-01-26 02:11:42 +0530

    Update timer.jsx

--- Assignment/set timer/timer/src/conponants/timer.jsx
+++ Assignment/set timer/timer/src/conponants/timer.jsx
@@ -3,10 +3,13 @@
 
 
 // export const Timer = (start, end , flag) => {
+    
 //     const [tim, setTim] = useState(0)
+
 //     setTim(start)
 
 //     const flags = useRef(flag)
+
 //         let id = setInterval(() => {
 //             setTim((p) => {
 //                 if (p === end) {
